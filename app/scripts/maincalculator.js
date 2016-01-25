cf4c685e46d84925960a02007ab13428bb6555fe
ArielAlvarado <alvarado.ariel.contact@gmail.com>
2016-01-25 07:33:10 -0400

    another one

---
+++ app/scripts/maincalculator.js
@@ -0,0 +1,60 @@
+function esSuma(tecla){
+	if (tecla == "+"){
+		return true;
+	}
+	return false;	
+}
+
+function esResta(tecla){
+	if (tecla == "-"){
+		return true;
+	}
+	return false;	
+}
+
+function esMulti(tecla){
+	if (tecla == "x"){
+		return true;
+	}
+	return false;	
+}
+
+function esDiv(tecla){
+	if (tecla == "\u00F7"){
+		return true;
+	}
+	return false;	
+}
+
+function esOperacion(tecla){
+	if (esMulti(tecla) || esResta(tecla) || esSuma(tecla) || esDiv(tecla) || tecla == "%"){
+		return true;
+	}
+	return false;
+}
+
+/* from https://stackoverflow.com/questions/6449611/how-to-check-whether-a-value-is-a-number-in-javascript-or-jquery */
+function esNumero(tecla) {
+	return !isNaN(parseFloat(tecla)) && isFinite(tecla);
+}
+
+function esLimpiaPantalla(tecla){
+	if (tecla == "AC" || tecla == "CA"){
+		return true;
+	}
+	return false;
+}
+
+function esSeparadorDecimal(tecla){
+	if (tecla == "."){
+		return true;
+	}
+	return false;
+}
+
+function esIgual(tecla){
+	if (tecla == "="){
+		return true;
+	}
+	return false;
+}
